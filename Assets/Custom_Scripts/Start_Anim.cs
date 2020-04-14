f27e485c355ce988467455fc9d5fb9e263b4ec18
Liu WenYue <LiuWY0912@gmail.com>
2020-04-13 22:14:17 -0400

    IP2: Added script that check when video is over

---
+++ Assets/Custom_Scripts/Start_Anim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Start_Anim : MonoBehaviour
+{
+    public VideoPlayer vid;
+
+
+    void Start() { vid.loopPointReached += CheckOver; }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        Debug.Log("Video Is Over");
+    }
+
+}
+
+
