57f2531cb97318a88c65d90eddf396219c1091c7
Liu WenYue <LiuWY0912@gmail.com>
2020-04-14 02:57:26 -0400

    IP2: Disabled video player after the anim finish

--- Assets/Custom_Scripts/Start_Anim.cs
+++ Assets/Custom_Scripts/Start_Anim.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class Start_Anim : MonoBehaviour
 {
     public VideoPlayer vid;
+    public GameObject videoRender;
 
 
     void Start() { vid.loopPointReached += CheckOver; }
@@ -13,8 +15,9 @@ public class Start_Anim : MonoBehaviour
     void CheckOver(UnityEngine.Video.VideoPlayer vp)
     {
         Debug.Log("Video Is Over");
+        vid.enabled = false;
+        videoRender.SetActive(false);
     }
-
 }
 
 
