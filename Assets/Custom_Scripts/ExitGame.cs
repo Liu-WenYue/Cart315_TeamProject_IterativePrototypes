59d9e929cd2e18dba8c302360f07d08610e0bb1c
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-04-14 23:29:08 -0400

    Changed exitgame script

--- Assets/Custom_Scripts/ExitGame.cs
+++ Assets/Custom_Scripts/ExitGame.cs
@@ -7,8 +7,8 @@ public class ExitGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Cursor.visible = true;
-       // Screen.lockCursor = false;
+        Cursor.visible = true;
+        Screen.lockCursor = false;
     }
 
     // Update is called once per frame
